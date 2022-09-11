4601919119df973b5fc1fe13f4eeb1e9043a56af
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-11 18:47:01 +0530

    [Anurag] Add: Calculate Lenght of Line

---
+++ LineComparison/Line_Comparison.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class Line_Comparison
+    {
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
+        public void LineComparison()
+        {
+
+
+            double lenght;
+
+            lenght = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine("Length of a given Line is: " + lenght);
+        }
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Program");
+
+            Line_Comparison obj = new Line_Comparison();
+
+            Console.WriteLine("x1");
+            obj.x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1");
+            obj.y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x2");
+            obj.x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y2");
+            obj.y2 = Convert.ToInt32(Console.ReadLine());
+
+            obj.LineComparison();
         }
     }
 }
